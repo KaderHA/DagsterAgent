7581c1337384d78e4b310e304a6f5a03d48f46a8
KaderHA <kader.hussein.abdi@norges-bank.no>
2024-07-19 14:54:39 +0200

    Removed everything to do with ADF

--- dagster/dagster_project/__init__.py
+++ dagster/dagster_project/__init__.py
@@ -1,15 +1,14 @@
 from dagster import Definitions, ScheduleDefinition, load_assets_from_modules
 
-from .assets import dagster_databricks_pipes, db_pipes, datafactory
-from .resources import databricks_client, adf_client
+from .assets import dagster_databricks_pipes, db_pipes
+from .resources import databricks_client
 
-all_assets = load_assets_from_modules([dagster_databricks_pipes, db_pipes, datafactory])
+all_assets = load_assets_from_modules([dagster_databricks_pipes, db_pipes])
 
 defs = Definitions(
     assets=all_assets,
     resources={
         "databricks": databricks_client.databricks_client_instance,
         "pipes_databricks": databricks_client.pipes_databricks_resource,
-        "adf_client": adf_client.adf_client_instance,
     },
 )
--- dagster/dagster_project/resources/adf_client.py
+++
@@ -1,8 +0,0 @@
-from azure.identity import AzureCliCredential
-from azure.mgmt.datafactory import DataFactoryManagementClient
-
-import os
-
-adf_client_instance = DataFactoryManagementClient(
-    AzureCliCredential(), os.getenv("SUBSCRIPTION_ID")
-)
